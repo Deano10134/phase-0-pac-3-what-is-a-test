ce12d7308912c97255dfcaed359a76bb9d589982
Dean Baberowski <deanbaberows@gmail.com>
2025-03-25 16:56:41 +1100

    Updated lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const height = 100;
+const message = `${name} is ${height} centermetres tall`;
 
 module.exports = {name, height, message};
+console.log(message);
+
